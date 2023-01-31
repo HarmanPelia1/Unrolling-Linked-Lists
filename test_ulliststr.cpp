45ba8f6a051d3c9a8185433addd5e871ccadb442
Harman Pelia <pelia@usc.edu>
2023-01-31 11:47:17 -0800

    Finished Product

--- test_ulliststr.cpp
+++ test_ulliststr.cpp
@@ -1,13 +1,53 @@
+#include <random>
 #include <string>
 #include <vector>
 #include <iostream>
 #include <sstream>
 
+
 #include "ulliststr.h"
+using namespace std; 
 
 //Use this file to test your ulliststr implementation before running the test suite
 
+ULListStr * makeList(std::vector<std::string> const & values, bool pushBack)
+{
+	// circular list is not copyable so we have to pass it by pointer
+	ULListStr * list = new ULListStr();
+
+	if(pushBack)
+	{
+		for(std::vector<std::string>::const_iterator valuesIter = values.cbegin(); valuesIter != values.cend(); ++valuesIter)
+		{
+			list->push_back(*valuesIter);
+		}
+	}
+	else
+	{
+		// push values in reverse from the front
+		for(std::vector<std::string>::const_reverse_iterator valuesIter = values.crbegin(); valuesIter != values.crend(); ++valuesIter)
+		{
+			list->push_front(*valuesIter);
+		}
+	}
+
+	return list;
+}
+
+
 int main(int argc, char* argv[])
 {
+	std::vector<std::string> content = {"sup", "er", "cal", "i", "frag", "il", "istic", "expi", "alo", "doc", "ious"};
+
+	ULListStr * list = makeList(content, true);
+
+	cout << list->get(9) << endl;
+	cout << list->get(10) << endl;
+
+
+	list->pop_back();
+
+	cout << "Final: " << list->get(9) << endl;
+
 
 }
--- ulliststr.cpp
+++ ulliststr.cpp
@@ -1,4 +1,3 @@
-foo
 #include <cstddef>
 #include <stdexcept>
 #include "ulliststr.h"
@@ -25,7 +24,134 @@ size_t ULListStr::size() const
   return size_;
 }
 
-// WRITE YOUR CODE HERE
+// CODEE
+
+void ULListStr::push_back(const std::string& val)
+{
+if(empty()){
+      Item* item1 = new Item();
+      item1->val[0] = val;
+      item1->last++;
+      head_ = item1;
+      tail_ = item1;
+    }
+  else if(tail_->last == ARRSIZE){
+    Item* item1 = new Item();
+    tail_->next = item1;
+    item1->prev = tail_;
+    tail_ = item1;
+    item1->val[0] = val;
+    item1->last++;
+  }
+  else{
+    tail_->val[tail_->last] = val;
+    tail_->last++;
+  }
+  size_++;
+}
+
+void ULListStr::pop_back()
+{
+  if(empty()){
+    return;
+  }
+  if(tail_->last - tail_->first > 1){
+        tail_->last--;
+ 
+  }
+  else{
+    if(head_ == tail_){
+      delete tail_;
+      head_ = nullptr;
+      tail_ = nullptr;
+    } 
+    else {
+      Item* temp = tail_;
+      tail_ = tail_->prev;
+      tail_->next = nullptr;
+      delete temp;
+    }
+  }
+  size_--;
+}
+
+void ULListStr::push_front(const std::string& val)
+{
+  if(empty()){
+    Item* new_item = new Item();
+    new_item->val[0] = val;
+    new_item->last++;
+    head_ = new_item;
+    tail_ = new_item;
+  }
+  else{
+    if(head_->first == 0){
+    Item* new_item = new Item();
+    new_item->val[0] = val;
+    new_item->last++;
+    head_->prev = new_item;
+    new_item->next = head_;
+    head_ = new_item;
+    }
+    else{
+      head_->val[head_->first - 1] = val;
+      head_->first--;
+    }
+  }
+  size_++;
+}
+
+void ULListStr::pop_front()
+{
+  if(empty()){
+    return;
+  }
+ else if(head_->last - head_->first == 1){
+    if(head_ == tail_){
+      delete head_;
+      head_ = nullptr;
+      tail_ = nullptr;
+  }
+    else{
+        Item* temp = head_;
+        head_ = head_->next;
+        head_->prev = nullptr;
+        delete temp;
+    }
+  }
+  else{
+    head_->first++;
+  }
+  size_--;
+}
+
+std::string const & ULListStr::front() const
+{
+  return head_->val[head_->first];
+}
+
+
+
+std::string const & ULListStr::back() const
+{
+  return tail_->val[tail_->last-1];
+}
+
+
+
+std::string* ULListStr::getValAtLoc(size_t loc) const
+{
+    Item* temp = head_;
+          while(loc >= temp->last - temp->first){//check if this is right
+            loc = loc - (temp->last - temp->first);
+            temp = temp->next;
+          }
+    return &temp->val[temp->first + loc];
+}
+
+
+//STOPPPPPP
+
 
 void ULListStr::set(size_t loc, const std::string& val)
 {
